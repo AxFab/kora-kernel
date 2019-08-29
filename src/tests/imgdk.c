e6f3f3fdbe3d555a921352f16f62fba9f25a4b54
Fabien Bavent <fabien.bavent@gmail.com>
2019-08-30 01:00:12 +0200

    Add support for VHD images on VFS testing tool

--- src/tests/imgdk.c
+++ src/tests/imgdk.c
@@ -55,13 +55,25 @@ void imgdk_release(inode_t *ino, off_t off, page_t pg);
 int imgdk_ioctl(inode_t *ino, int cmd, long *params);
 int imgdk_close(inode_t *ino);
 
+int vhd_read(inode_t *ino, void *data, size_t size, off_t offset);
+int vhd_write(inode_t *ino, const void *data, size_t size, off_t offset);
+page_t vhd_fetch(inode_t *ino, off_t off);
+void vhd_sync(inode_t *ino, off_t off, page_t pg);
+void vhd_release(inode_t *ino, off_t off, page_t pg);
+
+
 ino_ops_t imgdk_ino_ops = {
-    .read = (void*)imgdk_read,
-    .write = (void*)imgdk_write,
     .close = imgdk_close,
     .fetch = imgdk_fetch,
     .sync = imgdk_sync,
     .release = imgdk_release,
+};
+
+ino_ops_t vhd_ino_ops = {
+    .close = imgdk_close,
+    .fetch = vhd_fetch,
+    .sync = vhd_sync,
+    .release = vhd_release,
 };
 
 dev_ops_t imgdk_dev_ops = {
@@ -69,6 +81,70 @@ dev_ops_t imgdk_dev_ops = {
 };
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+PACK(struct footer_vhd {
+    char cookie[8];
+    uint32_t features;
+    uint16_t vers_maj;
+    uint16_t vers_min;
+    uint64_t offset;
+    uint32_t timestamp;
+    char creator_app[4];
+    uint16_t creator_vers_maj;
+    uint16_t creator_vers_min;
+    uint32_t creator_host;
+    uint64_t original_size;
+    uint64_t actual_size;
+    uint16_t cylinders;
+    uint8_t heads;
+    uint8_t sectors;
+    uint32_t type;
+    uint32_t checksum;
+    char uuid[16];
+    char state;
+});
+
+
+PACK(struct header_vhd {
+    char cookie[8];
+    uint64_t data_off;
+    uint64_t table_off;
+    uint16_t vers_maj;
+    uint16_t vers_min;
+    uint32_t entries_count;
+    uint32_t block_size;
+    uint32_t checksum;
+});
+
+
+struct vhd_info {
+    uint32_t block_size;
+    uint64_t data_off;
+    uint64_t table_off;
+    void *cache;
+};
+
+
+#define __swap16(v)  (  \
+            ((v & 0xff) << 8) |  \
+            ((v & 0xff00) >> 8) )
+
+#define __swap32(v)  (  \
+            ((v & 0xff) << 24) |  \
+            ((v & 0xff00) << 8) |  \
+            ((v & 0xff0000) >> 8) |  \
+            ((v & 0xff000000) >> 24) )
+
+#define __swap64(v)  (  \
+            ((v & 0xffLL) << 48) |  \
+            ((v & 0xff00LL) << 40) |  \
+            ((v & 0xff0000LL) << 24) |  \
+            ((v & 0xff000000LL) << 8) |  \
+            ((v & 0xff00000000LL) >> 8) |  \
+            ((v & 0xff0000000000LL) >> 24) |  \
+            ((v & 0xff000000000000) >> 40) |  \
+            ((v & 0xff00000000000000) >> 48) )
+
 
 int imgdk_open(const char *path, const char *name)
 {
@@ -80,26 +156,94 @@ int imgdk_open(const char *path, const char *name)
     ino->length = lseek(fd, 0, SEEK_END); // Optional
     // ino->lba = fd;
 
-    if (strcmp(strrchr(path, '.'), ".img") == 0) {
-        ino->dev->block = 512;
-    } else if (strcmp(strrchr(path, '.'), ".iso") == 0) {
-        ino->dev->block = 2048;
-        ino->dev->flags = VFS_RDONLY;
-    }
-
-    ino->dev->devclass = strdup("Image disk"); // Mandatory
-    // O ino->dev->devname
-    // O ino->dev->model
-    // O ino->dev->vendor
     ino->btime = cpu_clock(0);
     ino->ctime = ino->btime;
     ino->mtime = ino->btime;
     ino->atime = ino->btime;
 
-    ino->dev->ops = &imgdk_dev_ops;
     ino->ops = &imgdk_ino_ops;
-	ino->info = blk_create(ino, imgdk_read, imgdk_write);
-    // ino->info = blk_create(blk, imgdk_read, imgdk_write);
+    ino->dev->ops = &imgdk_dev_ops;
+
+    if (strcmp(strrchr(path, '.'), ".img") == 0) {
+        ino->dev->block = 512;
+    } else if (strcmp(strrchr(path, '.'), ".iso") == 0) {
+        ino->dev->block = 2048;
+        ino->dev->flags = VFS_RDONLY;
+    } else if (strcmp(strrchr(path, '.'), ".vhd") == 0) {
+        ino->dev->block = 512;
+
+        struct footer_vhd footer;
+        size_t pos = lseek(fd, -512, SEEK_END);
+        read(fd, &footer, sizeof(footer));
+
+        footer.offset = __swap64(footer.offset);
+        footer.vers_maj = __swap16(footer.vers_maj);
+        footer.vers_min = __swap16(footer.vers_min);
+        footer.timestamp = __swap32(footer.timestamp);
+        footer.creator_vers_maj = __swap16(footer.creator_vers_maj);
+        footer.creator_vers_min = __swap16(footer.creator_vers_min);
+        footer.original_size = __swap64(footer.original_size);
+        footer.actual_size = __swap64(footer.actual_size);
+        footer.cylinders = __swap16(footer.cylinders);
+        footer.type = __swap32(footer.type);
+        memcpy(ino->dev->id, footer.uuid, 16);
+
+        // Build Vendor string
+        char vname[5];
+        memcpy(vname, footer.creator_app, 4);
+        int i = 3;
+        vname[4] = '\0';
+        while (i > 0 && vname[i] == ' ')
+            vname[i--] = '\0';
+        char vendor[64];
+        snprintf(vendor, 64, "%s_%d.%d", vname, footer.creator_vers_maj, footer.creator_vers_min);
+        ino->dev->vendor = strdup(vendor);
+
+        if (footer.type == 2) {
+            ino->dev->model = strdup("VHD-Fixed");
+            ino->length -= 512;
+        } else if (footer.type == 3) {
+            ino->length = footer.actual_size;
+            ino->dev->model = strdup("VHD-Dynamic");
+
+            struct header_vhd header;
+
+            size_t pos = lseek(fd, footer.offset, SEEK_SET);
+            read(fd, &header, sizeof(header));
+
+            header.data_off = __swap64(header.data_off);
+            header.table_off = __swap64(header.table_off);
+            header.vers_maj = __swap16(header.vers_maj);
+            header.vers_min = __swap16(header.vers_min);
+            header.entries_count = __swap32(header.entries_count);
+            header.block_size = __swap32(header.block_size);
+
+            struct vhd_info *info = malloc(sizeof(struct vhd_info));
+            ino->info = info;
+            info->data_off = header.data_off;
+            info->table_off = header.table_off;
+            info->block_size = header.block_size;
+            info->cache = blk_create(ino, vhd_read, vhd_write);
+            ino->ops = &vhd_ino_ops;
+        } else {
+            vfs_close(ino);
+            close(fd);
+            return -1;
+        }
+    }
+
+    ino->dev->devclass = strdup("Image disk"); // Mandatory
+
+    char *dname = strchr(path, '/');
+    dname = dname == NULL ? path : dname + 1;
+    ino->dev->devname = strdup(dname);
+    // O ino->dev->devname
+    // O ino->dev->model
+    // O ino->dev->vendor
+
+    if (ino->ops == &imgdk_ino_ops)
+        ino->info = blk_create(ino, imgdk_read, imgdk_write);
+
     vfs_mkdev(ino, name);
     vfs_close(ino);
     return 0;
@@ -173,6 +317,87 @@ void imgdk_release(inode_t *ino, off_t off, page_t pg)
 }
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+int vhd_read(inode_t *ino, void *data, size_t size, off_t offset)
+{
+    int fd = ino->no;
+    struct vhd_info *info = ino->info;
+    splock_lock(&ino->lock);
+    while (size > 0) {
+        size_t idx = offset / info->block_size;
+        size_t bat_off = idx * 4 + info->table_off;
+        uint32_t bat_val;
+        lseek(fd, bat_off, SEEK_SET);
+        read(fd, &bat_val, 4);
+        size_t avail = MIN(size, info->block_size - offset % info->block_size);
+        if (bat_val == ~0) {
+            memset(data, 0, avail);
+        } else {
+            size_t dk_off = bat_val * 512 + 512 + offset % info->block_size;
+            lseek(fd, dk_off, SEEK_SET);
+            read(fd, data, avail);
+        }
+        size -= avail;
+        data = ADDR_OFF(data, avail);
+        offset -= avail;
+    }
+    splock_unlock(&ino->lock);
+    return 0;
+}
+
+int vhd_write(inode_t *ino, const void *data, size_t size, off_t offset)
+{
+    int fd = ino->no;
+    struct vhd_info *info = ino->info;
+    assert((ino->flags & VFS_RDONLY) == 0);
+    splock_lock(&ino->lock);
+    while (size > 0) {
+        size_t idx = offset / info->block_size;
+        size_t bat_off = idx * 4 + info->table_off;
+        uint32_t bat_val;
+        lseek(fd, bat_off, SEEK_SET);
+        read(fd, &bat_val, 4);
+        size_t avail = MIN(size, info->block_size - offset % info->block_size);
+        if (bat_val == ~0) {
+            // ... Need allocate !
+            // lseek -512 SEEK_END
+            // WRITE block_size !
+            // COPY FOOTER
+            // WRITE BAT entry !
+            return -1;
+        }
+
+        size_t dk_off = bat_val * 512 + 512 + offset % info->block_size;
+        lseek(fd, dk_off, SEEK_SET);
+        write(fd, data, avail);
+
+        size -= avail;
+        data = ADDR_OFF(data, avail);
+        offset -= avail;
+    }
+    splock_unlock(&ino->lock);
+    return 0;
+}
+
+page_t vhd_fetch(inode_t *ino, off_t off)
+{
+    struct vhd_info *info = ino->info;
+    return blk_fetch(info->cache, off);
+}
+
+void vhd_sync(inode_t *ino, off_t off, page_t pg)
+{
+    struct vhd_info *info = ino->info;
+    blk_sync(info->cache, off, pg);
+}
+
+void vhd_release(inode_t *ino, off_t off, page_t pg)
+{
+    struct vhd_info *info = ino->info;
+    blk_release(info->cache, off, pg);
+}
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 void imgdk_create(CSTR name, size_t size)
 {
     int zero = 0;
@@ -185,6 +410,86 @@ void imgdk_create(CSTR name, size_t size)
         kprintf(-1, "Unable to create image disk %s\n", name);
 }
 
+uint32_t vhd_checksum(char *buf, int len)
+{
+    uint32_t checksum = 0;
+    for (int i = 0; i < len; ++i) {
+        checksum += buf[i];
+    }
+    return __swap32(~checksum);
+}
+
+void vhd_create_dyn(CSTR name, size_t size)
+{
+    char buf[512];
+    int fd = open(name, O_WRONLY | O_BINARY | O_CREAT | O_TRUNC, 0644);
+
+    struct footer_vhd *footer = (void*)&buf;
+
+    int sectors = MIN(size / 512, 65535 * 16 * 255);
+    memset(buf, 0, 512);
+    memcpy(footer->cookie, "conectix", 8);
+    footer->features = 0x2000000;
+    footer->vers_maj = __swap16(1);
+    footer->vers_min = __swap16(0);
+    footer->offset = __swap32(512);
+    footer->timestamp = 0;
+    memcpy(footer->creator_app, "kora", 4);
+    footer->creator_vers_maj = __swap16(1);
+    footer->creator_vers_min = __swap16(0);
+    footer->creator_host = 0;
+    footer->original_size = __swap64(size);
+    footer->actual_size = __swap64(size);
+    if (sectors > 65535 * 16 * 63) {
+        footer->cylinders = __swap16(sectors / 16 / 255);
+        footer->heads = 16;
+        footer->sectors = 255;
+    } else {
+        int cys = sectors / 17;
+        footer->sectors = 17;
+        footer->heads = MAX(4, (cys + 1023) / 1024);
+        if (footer->heads > 16 || cys > 1024 * footer->heads) {
+            footer->sectors = 31;
+            footer->heads = 16;
+            cys = sectors / 31;
+        }
+        if (cys > 1024 * footer->heads) {
+            footer->sectors = 63;
+            footer->heads = 16;
+            cys = sectors / 63;
+        }
+        footer->cylinders = __swap16(cys / footer->heads);
+    }
+    footer->type = 3;
+    footer->uuid[0] = rand8();
+    footer->checksum = vhd_checksum(footer, sizeof(struct footer_vhd));
+    int blocks = size / (2 * _Mib_);
+    write(fd, buf, 512);
+    lseek(fd, 1536 + ALIGN_UP(blocks * 4, 512), SEEK_SET);
+    write(fd, buf, 512);
+    lseek(fd, 512, SEEK_SET);
+
+    struct header_vhd *header = (void*)&buf;
+
+    memset(buf, 0, 512);
+    write(fd, buf, 512);
+    memcpy(header->cookie, "cxsparse", 8);
+
+    header->data_off = __swap64(0);
+    header->table_off = __swap64(1536);
+    header->vers_maj = __swap16(1);
+    header->vers_min = __swap16(0);
+    header->entries_count = 0;
+    header->block_size = __swap32(2 * _Mib_);
+    header->checksum = vhd_checksum(header, sizeof(struct header_vhd));
+    write(fd, buf, 512);
+
+    memset(buf, 0, 512);
+    for (int i = 0; i < ALIGN_UP(blocks * 4, 512) / 512; ++i)
+        write(fd, buf, 512);
+
+    close(fd);
+}
 
 void imgdk_setup()
 {
